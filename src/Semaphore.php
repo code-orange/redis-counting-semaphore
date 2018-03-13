165f374c83b8ebbf4a07e084a235c3ce33b6eace
Tim van Dalen <Tim@timvdalen.nl>
2018-03-13 16:35:51 +0100

    Implement fair semaphore with locking, implementation is complete now

--- src/Semaphore.php
+++ src/Semaphore.php
@@ -2,6 +2,7 @@
 namespace CodeOrange\RedisCountingSemaphore;
 
 use Predis\Client;
+use Predis\Transaction\MultiExec;
 use Ramsey\Uuid\Uuid;
 
 /**
@@ -48,7 +49,7 @@ class Semaphore {
 			// We already have it
 			return true;
 		}
-		$acquired = $this->acquire_unfair();
+		$acquired = $this->acquire_fair_with_lock();
 		if ($acquired) {
 			return true;
 		} else {
@@ -70,7 +71,7 @@ class Semaphore {
 			// We didn't have it
 			return;
 		}
-		$this->release_unfair();
+		$this->release_fair();
 	}
 
 	/**
@@ -82,7 +83,7 @@ class Semaphore {
 		if (!$this->identifier) {
 			return false;
 		}
-		return true;
+		return $this->refresh_fair();
 	}
 
 	//<editor-fold desc="Methods as built up to in the book">
@@ -115,10 +116,106 @@ class Semaphore {
 		return $this->client->zrem($this->name, $id);
 	}
 	private function acquire_fair() {
+		$identifier = (string) Uuid::uuid4();
+		$cszet = $this->name . ':owner';
+		$ctr = $this->name . ':counter';
 
+		$now = time();
+
+		$transaction = $this->client->transaction();
+
+		// Time out old entries
+		$transaction->zremrangebyscore($this->name, '-inf', $now - $this->timeout);
+		$transaction->zinterstore($cszet, [$cszet, $this->name], ['weights' => [1, 0]]);
+
+		// Get the counter
+		$transaction->incr($ctr);
+		$result = $transaction->execute();
+		$counter = $result[count($result) - 1];
+
+		// Try to acquire the semaphore
+		$transaction = $this->client->transaction();
+		$transaction->zadd($this->name, [$identifier => $now]);
+		$transaction->zadd($cszet, [$identifier => $counter]);
+
+		// Check the rank to determine if we got the semaphore
+		$transaction->zrank($cszet, $identifier);
+		$result = $transaction->execute();
+		$rank = $result[count($result) - 1];
+		if ($rank < $this->limit) {
+			// We got it!
+			$this->identifier = $identifier;
+			return true;
+		}
+
+		// We didn't get the semaphore, clean out the bad data
+		$transaction = $this->client->transaction();
+		$transaction->zrem($this->name, $identifier);
+		$transaction->zrem($cszet, $identifier);
+		$transaction->execute();
+
+		return false;
+	}
+	private function release_fair() {
+		$id = $this->identifier;
+		$this->identifier = null;
+
+		$transaction = $this->client->transaction();
+		$transaction->zrem($this->name, $id);
+		$transaction->zrem($this->name . ':owner', $id);
+		return $transaction->execute()[0];
+	}
+	private function refresh_fair() {
+		if ($this->client->zadd($this->name, [$this->identifier => time()])) {
+			// We lost it
+			$this->release_fair();
+			return false;
+		}
+		// We still have it
+		return true;
 	}
 	private function acquire_fair_with_lock() {
+		$identifier = $this->acquire_lock(0.01);
+		if ($identifier) {
+			try {
+				return $this->acquire_fair();
+			} finally {
+				$this->release_lock($identifier);
+			}
+		}
+		return false;
+	}
 
+	// From section 6.2 of the book
+	public function acquire_lock($acquire_timeout = 10) {
+		$identifier = (string)Uuid::uuid4();
+
+		$end = time() + $acquire_timeout;
+		while (time() < $end) {
+			$res = $this->client->setnx('lock:' . $this->name, $identifier);
+			if ($res) {
+				return $identifier;
+			}
+			sleep(0.001);
+		}
+		return false;
+	}
+	public function release_lock($id) {
+		$lockname = 'lock:' . $this->name;
+
+		$res = $this->client->transaction(['watch' => $lockname, 'cas' => true, 'retry' => 1000], function (MultiExec $t) use ($id, $lockname) {
+			$value = $t->get($lockname);
+			if ($value === $id) {
+				$t->multi();
+				$t->del([$lockname]);
+			}
+		});
+		if ($res) {
+			return true;
+		} else {
+			// We didn't execute anything, so we've lost the lock
+			return false;
+		}
 	}
 	//</editor-fold>
 
